3e29ea36ceb43f03c0956f7d2e526abb5a1e56b9
kelvin-daniel <kaymutor@gmail.com>
2020-10-31 15:41:32 +0300

    source file

--- block/urls.py
+++ block/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'',include('blocc.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.logout, {"next_page":'/'},name="logout"),
+    url(r'^tinymce',include('tinymce.urls')),
 ]
