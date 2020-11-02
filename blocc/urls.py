e81e3a5451273a834e3ebc919ffcd69157740d4f
kelvin-daniel <kaymutor@gmail.com>
2020-11-02 13:11:49 +0300

    add urls

--- blocc/urls.py
+++ blocc/urls.py
@@ -12,17 +12,9 @@ urlpatterns=[
     path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
     path('notifications',views.notification, name='notifications'),
     path('blog',views.blog, name='blog'),
-    path('health',views.health, name='health'),
-    path('authorities',views.authorities, name='authorities'),
-    path('businesses',views.businesses, name='businesses'),
     re_path('view/blog/(\d+)',views.view_blog,name='view_blog'),
     path('my-profile/',views.my_profile, name='my-profile'),
-    re_path('user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
-    path('new/blogpost',views.new_blogpost, name='new-blogpost'),
-    path('new/business',views.new_business, name='new-business'),
     path('create/profile',views.create_profile, name='create-profile'),
-    path('new/notification',views.new_notification, name='new-notification'),
-    path('update/profile',views.update_profile, name='update-profile'),
     path('search/',views.search_results, name='search_results'),
 
 ]
--- blocc/views.py
+++ blocc/views.py
@@ -109,30 +109,6 @@ def blog(request):
     return render(request,'blog.html',{"blogposts":blogposts, "form":form})
 
 @login_required(login_url='/accounts/login/')
-def health(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    healthservices = Health.objects.filter(neighbourhood=profile.neighbourhood)
-
-    return render(request,'health.html',{"healthservices":healthservices})
-
-@login_required(login_url='/accounts/login/')
-def authorities(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    authorities = Authorities.objects.filter(neighbourhood=profile.neighbourhood)
-
-    return render(request,'authorities.html',{"authorities":authorities})
-
-@login_required(login_url='/accounts/login/')
-def businesses(request):
-    current_user=request.user
-    profile=Profile.objects.get(username=current_user)
-    businesses = Business.objects.filter(neighbourhood=profile.neighbourhood)
-
-    return render(request,'businesses.html',{"businesses":businesses})
-
-@login_required(login_url='/accounts/login/')
 def view_blog(request,id):
     current_user = request.user
 
@@ -176,57 +152,6 @@ def my_profile(request):
 
     return render(request,'user_profile.html',{"profile":profile, "form":form})
 
-
-@login_required(login_url='/accounts/login/')
-def user_profile(request,username):
-    user = User.objects.get(username=username)
-    profile =Profile.objects.get(username=user)
-    
-
-    return render(request,'user_profile.html',{"profile":profile})
-
-@login_required(login_url='/accounts/login/')
-def new_blogpost(request):
-    current_user=request.user
-    profile =Profile.objects.get(username=current_user)
-
-    if request.method=="POST":
-        form =BlogPostForm(request.POST,request.FILES)
-        if form.is_valid():
-            blogpost = form.save(commit = False)
-            blogpost.username = current_user
-            blogpost.neighbourhood = profile.neighbourhood
-            blogpost.avatar = profile.avatar
-            blogpost.save()
-
-        return HttpResponseRedirect('/blog')
-
-    else:
-        form = BlogPostForm()
-
-    return render(request,'blogpost_form.html',{"form":form})
-
-@login_required(login_url='/accounts/login/')
-def new_business(request):
-    current_user=request.user
-    profile =Profile.objects.get(username=current_user)
-
-    if request.method=="POST":
-        form =BusinessForm(request.POST,request.FILES)
-        if form.is_valid():
-            business = form.save(commit = False)
-            business.owner = current_user
-            business.neighbourhood = profile.neighbourhood
-            business.save()
-
-        return HttpResponseRedirect('/businesses')
-
-    else:
-        form = BusinessForm()
-
-    return render(request,'business_form.html',{"form":form})
-
-
 @login_required(login_url='/accounts/login/')
 def create_profile(request):
     current_user=request.user
@@ -242,53 +167,6 @@ def create_profile(request):
 
         form = ProfileForm()
     return render(request,'profile_form.html',{"form":form})
-
-@login_required(login_url='/accounts/login/')
-def new_notification(request):
-    current_user=request.user
-    profile =Profile.objects.get(username=current_user)
-
-    if request.method=="POST":
-        form =notificationsForm(request.POST,request.FILES)
-        if form.is_valid():
-            notification = form.save(commit = False)
-            notification.author = current_user
-            notification.neighbourhood = profile.neighbourhood
-            notification.save()
-
-            if notification.priority == 'High Priority':
-                send_priority_email(profile.name,profile.email,notification.title,notification.notification,notification.author,notification.neighbourhood)
-
-        return HttpResponseRedirect('/notifications')
-
-
-    else:
-        form = notificationsForm()
-
-    return render(request,'notifications_form.html',{"form":form})
-
-@login_required(login_url='/accounts/login/')
-def update_profile(request):
-    current_user=request.user
-    if request.method=="POST":
-        instance = Profile.objects.get(username=current_user)
-        form =ProfileForm(request.POST,request.FILES,instance=instance)
-        if form.is_valid():
-            profile = form.save(commit = False)
-            profile.username = current_user
-            profile.save()
-
-        return redirect('Index')
-
-    elif Profile.objects.get(username=current_user):
-        profile = Profile.objects.get(username=current_user)
-        form = ProfileForm(instance=profile)
-    else:
-        form = ProfileForm()
-
-    return render(request,'update_profile.html',{"form":form})
-
-
 
 @login_required(login_url='/accounts/login/')
 def search_results(request):
